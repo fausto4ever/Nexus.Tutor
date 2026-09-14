35eeb989c769219b3c9238f84d9d68a1c9a2711e
fausto4ever <shindarked@gmail.com>
2026-09-14 10:51:23 -0600

    chore: cache PWA Nexus.Tutor 0.1.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='nexus-tutor-0.1.1';
-const ASSETS=['./','./index.html','./styles.css','./config.js','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='nexus-tutor-0.1.2';
+const ASSETS=['./','./index.html','./styles.css','./config.js','./app.js','./coordinates.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
