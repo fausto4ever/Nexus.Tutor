8db433d58b80e01660ca27382d1ae5186ad3553e
fausto4ever <shindarked@gmail.com>
2026-09-14 22:01:07 -0600

    chore: imprimir css optimizado legible

--- scripts/print-clean-css.mjs
+++ scripts/print-clean-css.mjs
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises';
 import CleanCSS from 'clean-css';
 
 const css=await fs.readFile(new URL('../css/nexus-tutor.css',import.meta.url),'utf8');
-const result=new CleanCSS({level:2,sourceMap:false}).minify(css);
+const result=new CleanCSS({level:2,sourceMap:false,format:'beautify'}).minify(css);
 if(result.errors.length)throw new Error(result.errors.join('\n'));
 console.log('---CSS_CANDIDATE_BEGIN---');
 console.log(result.styles);
