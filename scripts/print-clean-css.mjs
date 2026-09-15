dce1249e6ae2b4b2d03514bc183d12ed57562863
fausto4ever <shindarked@gmail.com>
2026-09-14 21:59:51 -0600

    chore: generar candidato css optimizado

---
+++ scripts/print-clean-css.mjs
@@ -0,0 +1,9 @@
+import fs from 'node:fs/promises';
+import CleanCSS from 'clean-css';
+
+const css=await fs.readFile(new URL('../css/nexus-tutor.css',import.meta.url),'utf8');
+const result=new CleanCSS({level:2,sourceMap:false}).minify(css);
+if(result.errors.length)throw new Error(result.errors.join('\n'));
+console.log('---CSS_CANDIDATE_BEGIN---');
+console.log(result.styles);
+console.log('---CSS_CANDIDATE_END---');
