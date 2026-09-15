fbba86c8da7ca60d601bbe0c1a57d90b903fb2ac
fausto4ever <shindarked@gmail.com>
2026-09-14 22:06:48 -0600

    chore: retirar impresor css temporal

--- scripts/print-clean-css.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs/promises';
-import CleanCSS from 'clean-css';
-
-const css=await fs.readFile(new URL('../css/nexus-tutor.css',import.meta.url),'utf8');
-const result=new CleanCSS({level:2,sourceMap:false,format:'beautify'}).minify(css);
-if(result.errors.length)throw new Error(result.errors.join('\n'));
-console.log('---CSS_CANDIDATE_BEGIN---');
-console.log(result.styles);
-console.log('---CSS_CANDIDATE_END---');
